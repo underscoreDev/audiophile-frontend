198150e9a865f9d1cc2ca1531468eda6f21c29a3
Godswill Edet <97456640+underscoreDev@users.noreply.github.com>
2022-08-25 00:52:35 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import type { AppProps } from "next/app";
 import { Global } from "utils/globalStyles";
 import { persistStore } from "redux-persist";
 import Footer from "components/footer/footer";
-import Navbar from "components/navbar/navbar";
 import * as _ from "styled-components/cssprop";
 import { PersistGate } from "redux-persist/integration/react";
 import ResponsiveAppBar from "components/navbar";
@@ -18,7 +17,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <ResponsiveAppBar/>
         {/* <Navbar /> */}
         <Component {...pageProps} />
-        {/* <Footer /> */}
+        <Footer /> 
       </PersistGate>
     </Provider>
   );
